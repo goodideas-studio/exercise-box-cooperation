5eb4c68210e6f8a1f856610074f9b52fd7c6ba09
ellstang <lilyc633@gmail.com>
2018-05-17 15:17:55 +0800

    1. created GameRecordArray to store PlayRecord

--- team-gavin/Starbug/View Controllers/GameRecordsViewController.swift
+++ team-gavin/Starbug/View Controllers/GameRecordsViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class GameRecordsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
- var tableViewCellTitle = ["game1", "game1","game1", "game1"]
-  
+  var gameRecordArray: [PlayRecord] = []
   
   @IBOutlet weak var gameRecordTableView: UITableView!
 
