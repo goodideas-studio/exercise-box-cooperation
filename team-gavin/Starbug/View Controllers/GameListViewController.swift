218f9c274beca02547ee7648fae7036507a5717e
ellstang <lilyc633@gmail.com>
2018-05-17 13:05:56 +0800

    1. created "GameListViewController" file

---
+++ team-gavin/Starbug/View Controllers/GameListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameListViewController.swift
+//  Starbug
+//
+//  Created by 唐嘉伶 on 2018/5/17.
+//  Copyright © 2018 PinguMac. All rights reserved.
+//
+
+import UIKit
+
+class GameListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
